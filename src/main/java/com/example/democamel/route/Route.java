07aa6cb24c45c0a862a22fa8f8fe776ef5f3b646
patrickblot78@gmail.com <Swz788X@>
2021-03-10 18:03:11 +0100

    Mise au propre du crud

--- src/main/java/com/example/democamel/route/Route.java
+++ src/main/java/com/example/democamel/route/Route.java
@@ -3,15 +3,21 @@ package com.example.democamel.route;
 import com.example.democamel.errors.CustomException;
 import com.example.democamel.model.Dummy;
 import com.example.democamel.service.DummyService;
+import com.example.democamel.service.DummyServiceImpl;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestParamType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+
+import java.awt.print.Book;
 
 @Component
 public class Route extends RouteBuilder {
@@ -20,15 +26,8 @@ public class Route extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        //CamelContext context = new DefaultCamelContext();
-        Dummy dummy = new Dummy();
-        dummy.setId(1);
-        dummy.setName("dummy_1");
-        dummyService.create(dummy);
-        Dummy dummy2 = new Dummy();
-        dummy2.setId(2);
-        dummy2.setName("dummy_2");
-        dummyService.create(dummy2);
+        dummyService.create(new Dummy(1, "dummy_1"));
+        dummyService.create(new Dummy(2, "dummy_2"));
         //
         onException(CustomException.class)
                 .maximumRedeliveries(2)
@@ -39,30 +38,46 @@ public class Route extends RouteBuilder {
                 .handled(true)
                 .to("bean:camelRetryExceptionHandlerService?method=handleCustomException");
 
-         /*from("jetty:http://0.0.0.0:9080/testtolog").to("log:write to log");
-        from("jetty:http://0.0.0.0:9080/probe")
-                .routeId("first-route")
-                .to("direct:remoteService");
-        from("direct:remoteService")
-                .routeId("seconde-route")
-                .tracing()
-                .log(">>> 1")
-                .log(">>> 2")
-                .transform().simple("I'm Alive !")
-                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200));
-       */
-        restConfiguration().component("jetty").host("localhost").port(9080)
+        restConfiguration()
+                .component("jetty").host("localhost").port(9080)
                 // use camel-http as rest client
                 .producerComponent("http")
                 // produce json
                 .bindingMode(RestBindingMode.json);
-        //
-        from("direct:probe").to("rest:get:probe");
-        rest("/probe").get().route().transform().simple("I'm Alive !")
-                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200));
-        // read
-        from("direct:find-dummy").to("rest:get:dummys/{id}");
-        rest("/dummys/").get("{id}").route().to("mock:input")
+
+        rest("/dummys")
+                .get("/{id}")
+                    .description("Gets the Dummy by ID")
+                    .param()
+                        .name("id")
+                        .type(RestParamType.path)
+                        .description("The id of the user to get")
+                        .dataType("number")
+                    .endParam()
+                    .produces("application/json")
+                    .outType(Dummy.class)
+                    .to("direct:getDummy")
+                .get("/probe")
+                    .to("direct:getProbe")
+                .delete("/{id}")
+                    .param()
+                        .name("id")
+                        .type(RestParamType.path)
+                        .description("Deletes the user from database given an ID")
+                        .dataType("number")
+                    .endParam()
+                    .produces("application/json")
+                    .outType(String.class)
+                    .to("direct:deleteDummy")
+                .post()
+                    .type(Dummy.class)
+                    .outType(Dummy.class)
+                    .description("Creates a Dummy")
+                    .consumes("application/json")
+                    .produces("application/json")
+                    .to("direct:createDummy");
+
+        from("direct:getDummy").to("mock:input")
                 .to("bean:dummyService?method=findDummy(${header.id})")
                 .process(new Processor() {
                     public void process(Exchange exchange) throws Exception {
@@ -71,9 +86,17 @@ public class Route extends RouteBuilder {
                     }
                 });
 
-        // create
-        from("direct:create-dummy").to("rest:post:dummy");
-        rest("/dummy").post()
-                .to("bean:dummyService?method=create(${header.body})");
+        from("direct:getProbe").transform().simple("I'm Alive !")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200));
+
+        from("direct:createDummy")
+                .to("mock:input")
+                .to("bean:dummyService?method=create(${body})")
+                .transform().simple("create");
+
+        from("direct:deleteDummy")
+                .to("mock:input")
+                .to("bean:dummyService?method=delete(${header.id})")
+                .transform().simple("delete");
     }
 }
--- src/test/java/com/example/democamel/route/RouteTest.java
+++ src/test/java/com/example/democamel/route/RouteTest.java
@@ -3,10 +3,7 @@ package com.example.democamel.route;
 import com.example.democamel.dao.DummyDao;
 import com.example.democamel.model.Dummy;
 import com.example.democamel.service.DummyServiceImpl;
-import org.apache.camel.CamelContext;
-import org.apache.camel.EndpointInject;
-import org.apache.camel.ProducerTemplate;
-import org.apache.camel.ServiceStatus;
+import org.apache.camel.*;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
@@ -16,8 +13,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.TestPropertySource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @CamelSpringBootTest
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -51,19 +47,24 @@ public class RouteTest {
     @Order(2)
     public void testProbe() throws Exception {
         template = camelContext.createProducerTemplate();
-        String out = template.requestBody("direct:probe", null, String.class);
-        assertEquals("\"I'm Alive !\"", out);
+        String out = template.requestBody("direct:getProbe", null, String.class);
+        assertEquals("I'm Alive !", out);
         //
         template.stop();
     }
 
     @Test
     @Order(3)
-    public void testDummy() throws Exception {
-        dummy = new Dummy();
-        dummy.setId(3);
-        dummy.setName("dummy_3");
-        //mockDummyService.
+    public void testCreateDummy() throws Exception {
+        template = camelContext.createProducerTemplate();
+        template.sendBody("direct:create-dummy", new Dummy(3, "dummy_3"));
+       // String out = template.sendBodyAndHeader()
+
+                /*to("rest:post:users/lives")
+                .withHeader(Exchange.CONTENT_TYPE, "application/json")
+                .withBody(md).request(String.class);*/
+
+        //assertNotNull(out);
     }
 
 }
